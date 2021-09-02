6a963539326ce44442e427cca072f3e6fc17bf0a
ngoolDev <ngooldev@gmail.com>
2021-09-02 23:50:58 +0200

    Added final Changes

--- Core/Entities/StockImage.cs
+++
@@ -1,9 +0,0 @@
-namespace Core.Entities
-{
-    public class StockImage
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public byte[] ImageData { get; set; }
-    }
-}
